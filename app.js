3e3a7b66e722614efd60a88f90ffcf04ae79a335
FN510 <f.ngemoh@warwick.ac.uk>
2021-06-24 23:29:46 +0100

    working on single session fetch api

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+	extended: true
+  }));
 /* -- database --- */
 const mongoose = require('mongoose');
+var ObjectId = require('mongoose').Types.ObjectId; 
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=> {
 	console.log("connected to db...");
@@ -37,16 +42,39 @@ const Session = mongoose.model('Session', sessionSchema);
 
 // Persists a session study event
 app.post("/courses/:courseId", (req, res)=> {
-	res.send(req)
+	// check user param in header and user exists
+	
+
 });
 
 // Fetches course lifetime statistics
 app.get("/courses/:courseId", (req, res)=> {
-   
+
 });
 
 // Fetches a single study session
-app.get("/courses/:courseId/sessions/:sessionId");
+app.get("/courses/:courseId/sessions/:sessionId", (req, res) => {
+	let courseId = req.params.courseId;
+	let sessionId = req.params.sessionId;
+	let userId = req.header("X-User-Id");
+	let sessionQuery = {
+		_id: new ObjectId(sessionId),
+		courseId: new ObjectId(courseId),
+		userId: new ObjectId(userId)
+	}
+	Session.findOne(sessionQuery)
+	.then(data=> {
+		if (typeof data == 'object' && data!=null) {
+			let result = {
+				"sessionId": data._id,
+				"totalModulesStudied": data.totalModulesStudied,
+				"averageScore": data.averageScore,
+				"timeStudied": data.timeStudied
+			};
+			res.json(result);
+		}
+	})
+});
 
 
 app.listen(3000, ()=> {
