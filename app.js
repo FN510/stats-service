c63a60117a5bf5fe29f95759fceecf8f04210274
FN510 <f.ngemoh@warwick.ac.uk>
2021-06-24 20:31:12 +0100

    add mongo database connection and schemas

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
-let express = require("express");
+const express = require("express");
+const app = express();
+/* -- database --- */
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> {
+	console.log("connected to db...");
+})
+.catch((err)=> {
+	console.log("db connection failed");
+});
 
-var app = express();
+
+// Each course has modules which are studied in sessions by a user
+const userSchema = new mongoose.Schema({
+	name: String
+});
+const User = mongoose.model('User', userSchema);
+
+const courseSchema = new mongoose.Schema({
+	name: String
+});
+const Course = mongoose.model('Course', courseSchema);
+
+// Schema for a study session
+const sessionSchema = new mongoose.Schema({
+	userId: String,
+	courseId: String,
+	totalModulesStudied: Number,
+	averageScore: Number,
+	timeStudied: Number
+});
+const Session = mongoose.model('Session', sessionSchema);
+
+
+//---- routes ----
 
 // Persists a session study event
-app.post("/courses/:courseId");
+app.post("/courses/:courseId", (req, res)=> {
+	res.send(req)
+});
 
 // Fetches course lifetime statistics
 app.get("/courses/:courseId", (req, res)=> {
