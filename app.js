5a37a2a53c807b7f6425ad27a5a9e2accc27a05e
FN510 <f.ngemoh@warwick.ac.uk>
2021-06-25 15:46:55 +0100

    working on POST /course/courseId

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
-	extended: true
-  }));
+app.use(bodyParser.json())
 /* -- database --- */
 const mongoose = require('mongoose');
 var ObjectId = require('mongoose').Types.ObjectId; 
@@ -29,6 +27,64 @@ mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUn
 app.post("/courses/:courseId", (req, res)=> {
 	// check user param in header and user exists
 	
+	let userId = req.header("X-User-Id");
+	if (!userId) {
+		res.status(400).json({"error":"X-User-Id is not defined"})
+	} else {
+		// check user exists
+		User.findById(userId)
+		.then(data=> {
+			if (data!=null) {
+				console.log("user");
+				// check course exists
+				let courseId = req.params.courseId;
+				Course.findById(courseId)
+				.then(data=> {
+					if (data!=null) {
+						console.log("course");
+						// check json body for session details
+						if (req.body.hasOwnProperty("stats diff")) {
+							let details = req.body["stats diff"]
+							let sessionProps = ["sessionId", "totalModulesStudied", "averageScore", "timeStudied"];
+							sessionProps.forEach(p=> {
+								if (!details.hasOwnProperty(p)) {
+									res.status(400).json({"error":"stats diff object is missing key: "+ p});
+								}
+							});
+							// check sessionId doesn't already exist
+							Session.findById(details.sessionId)
+							.then(data=> {
+								if (data!=null) {
+									res.status(400).json({"error": "SessionId already exists"})
+								}
+							})
+							console.log("good post");
+							let postSession = new Session({
+								_id: new ObjectId(details.sessionId),
+								userId: new ObjectId(userId),
+									// courseId: new ObjectId(details.courseId),
+									// totalModulesStudied: parseInt(details.totalModulesStudied),
+									// averageScore: parseInt(details.averageScore),
+									// timeStudied: parseInt(details.timeStudied)
+							});
+							postSession.save()
+							.then(()=> res.status(201).json({"status": "OK"}))
+							.catch((err)=> res.status(500).json({"error": err}));
+						} else {
+							res.status(400).json({"error":"Request body is missing key:'status diff'"});
+						}
+					} else {
+						res.status(404).json({"error":"Course not found"});
+					}
+				})
+			} else {
+				res.status(404).json({"error":"User not found"});
+			}
+		})
+	}
+	
+	
+	
 
 });
 
@@ -42,6 +98,9 @@ app.get("/courses/:courseId/sessions/:sessionId", (req, res) => {
 	let courseId = req.params.courseId;
 	let sessionId = req.params.sessionId;
 	let userId = req.header("X-User-Id");
+	if (!userId) {
+		res.status(400).json({"error":"X-User-Id is not defined"})
+	}
 	let sessionQuery = {
 		_id: new ObjectId(sessionId),
 		courseId: new ObjectId(courseId),
@@ -58,7 +117,7 @@ app.get("/courses/:courseId/sessions/:sessionId", (req, res) => {
 			};
 			res.json(result);
 		} else {
-			res.status(404).send("Session not found");
+			res.status(404).json({"error:":"Session not found"});
 		}
 	})
 });
