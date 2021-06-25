9522c1da3e39de33442ac0e53852512c5afe7723
FN510 <f.ngemoh@warwick.ac.uk>
2021-06-25 17:20:00 +0100

    do aggregate api and create seed file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json())
+let aggStats = require('./aggStats');
 /* -- database --- */
 const mongoose = require('mongoose');
 var ObjectId = require('mongoose').Types.ObjectId; 
@@ -10,7 +11,7 @@ const Session = require("./models/session");
 const Course = require("./models/course");
 const User = require("./models/user");
 // db driver
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://testUser:S8c42xHbXB6MQem@cluster0.csliv.mongodb.net/stats?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=> {
 	console.log("connected to db...");
 })
@@ -88,10 +89,46 @@ app.post("/courses/:courseId", (req, res)=> {
 
 });
 
-// Fetches course lifetime statistics
+// Fetches course lifetime statistics (sessions)
 app.get("/courses/:courseId", (req, res)=> {
+	let courseId = req.params.courseId;
+	let userId = req.header("X-User-Id");
+	if (!userId) {
+		res.status(400).json({"error":"X-User-Id is not defined"})
+	} else {
+		// check user exists
+		User.findById(userId)
+		.then(data=> {
+			if (data!=null) {
+				// check course exists
+				Course.findById(courseId)
+				.then(data=> {
+					if (data!=null) {
+						// Look for sessions w/ course+user
+						Session.find({
+							userId: userId,
+							courseId: courseId,
+						})
+						.then(data=> {
+							if (data.length>0) {
+								res.json(aggStats(data));
+							}
+						})
 
+
+					} else {
+						res.status(404).json({"error":"Course not found"});
+					}
+				})
+			} else {
+				res.status(404).json({"error":"User not found"});
+			}
+		})
+	}
 });
+
+
+
 
 // Fetches a single study session
 app.get("/courses/:courseId/sessions/:sessionId", (req, res) => {
---
+++ seed.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+var ObjectId = require('mongoose').Types.ObjectId; 
+// db models
+const Session = require("./models/session");
+const Course = require("./models/course");
+const User = require("./models/user");
+// db driver
+mongoose.connect('mongodb+srv://testUser:S8c42xHbXB6MQem@cluster0.csliv.mongodb.net/stats?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> {
+	console.log("connected to db...");
+})
+.catch((err)=> {
+	console.log("db connection failed");
+});
+// users
+// var alice = new User({name:"Alice"});
+// alice.save();
+// var bob = new User({name:"Bob"});
+// bob.save()
+// var chip = new User({name:"Chip"});
+// chip.save();
+
+// courses
+var maths = new Course({name: "Maths"});
+maths.save();
+var english = new Course({name: "English"});
+english.save();
+
+// sessions
+User.findOne({name:"Alice"})
+.then(data=> {
+    let userId = data._id;
+    Course.findOne({name: "Maths"})
+    .then(data=> {
+        let courseId = data._id;
+        Session.insertMany([
+            {userId: userId, courseId: courseId, totalModulesStudied: 11, averageScore: 91, timeStudied: 60*1000*180},
+            {userId: userId, courseId: courseId, totalModulesStudied: 20, averageScore: 71, timeStudied: 60*1000*73},
+            {userId: userId, courseId: courseId, totalModulesStudied: 5, averageScore: 61, timeStudied: 60*1000*120},
+        ]).then(console.log("saved"));
+    }) 
+    
+});
+
+User.findOne({name:"Chip"})
+.then(data=> {
+    let userId = data._id;
+    Course.findOne({name: "English"})
+    .then(data=> {
+        let courseId = data._id;
+        Session.insertMany([
+            {userId: userId, courseId: courseId, totalModulesStudied: 3, averageScore: 50, timeStudied: 60*1000*10},
+            {userId: userId, courseId: courseId, totalModulesStudied: 1, averageScore: 99, timeStudied: 60*1000*5},
+            {userId: userId, courseId: courseId, totalModulesStudied: 19, averageScore: 82, timeStudied: 60*1000*120},
+        ]).then(console.log("saved"));
+    }) 
+    
+})
+
+User.findOne({name:"Bob"})
+.then(data=> {
+    let userId = data._id;
+    Course.findOne({name: "English"})
+    .then(data=> {
+        let courseId = data._id;
+        Session.insertMany([
+            {userId: userId, courseId: courseId, totalModulesStudied: 12, averageScore: 71, timeStudied: 60*1000*100},
+            {userId: userId, courseId: courseId, totalModulesStudied: 5, averageScore: 64, timeStudied: 60*1000*20},
+            {userId: userId, courseId: courseId, totalModulesStudied: 6, averageScore: 63, timeStudied: 60*1000*60},
+        ]).then(console.log("saved"));
+    }) 
+    
+})
