3ae996b50ecd9e1390321f68ccacdc9d8ee96b2a
FN510 <f.ngemoh@warwick.ac.uk>
2021-06-26 16:46:33 +0100

    update readme with testing data

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Stats Service
 
+## My Comments
+- I assumed the averageScore should be to 1 dp
+- I assumed the request body for saving a new session should contain a key 'status diff'
+- I used mongoDBs ObjectID as IDs not a uuid
+- I could/should do the aggregation at the database level via mongoose for speed
+
 ## To Run
 From  root of folder:
 ```
@@ -7,7 +13,7 @@ npm start
 ```
 
 ## APIs
-The express.js framework is being used to faciliate the API routing.
+The [express.js](https://expressjs.com/) framework is being used to faciliate the API routing.
 
 ### GET /courses/:courseId
 Fetches course lifetime statistics for a user
@@ -29,7 +35,7 @@ example status 200 response:
 }
 ```
 
-status 404 response
+example status 404 response
 ```JSON
 {
 "error": "Session not found"
@@ -44,7 +50,7 @@ status 400 response
 ```
 
 ## Data Storage
-The application is connected to an cloud instance of mongoDB, a noSQL database. A schema has been defined for a User, Course and Session. The Ids of the objects are [ObjectID](https://docs.mongodb.com/manual/reference/method/ObjectId/)s
+The application is connected to a cloud instance of mongoDB, a noSQL database. A schema has been defined for a User, Course and Session. The Ids of the objects are [ObjectID](https://docs.mongodb.com/manual/reference/method/ObjectId/)s. The [mongoose](https://mongoosejs.com/) driver is being used as interface between mongodb and node.js.
 
 ### User
 ```javascript
@@ -75,6 +81,9 @@ const sessionSchema = new mongoose.Schema({
 There is a [postman](http://postman.com) collection in the test folder. Each request in the postman collection has a series of tests that are run when a request is sent. A global variable url should be set http://localhost:3000 in postman.
 
 ### Some Data
+
+The seed.js file was used to put some data into the database.
+
 | Course  | courseId                 |
 |---------|--------------------------|
 | Maths   | 60d600393192904f2098442f |
@@ -86,7 +95,15 @@ There is a [postman](http://postman.com) collection in the test folder. Each req
 |---------|--------------------------|
 | Alice   | 60d5fabf3cb70946c077f221 |
 | Bob     | 60d5fabf3cb70946c077f222 |
-| Charlie | 60d5fabf3cb70946c077f223 |
+| Chip    | 60d5fabf3cb70946c077f223 |
+
+
+
+| User  | Courses        |
+|-------|----------------|
+| Alice | Maths          |
+| Bob   | Maths, English |
+| Chip  | English        |
 
 
 | SessionId                | userId                   | courseId                 |
@@ -94,8 +111,15 @@ There is a [postman](http://postman.com) collection in the test folder. Each req
 | 60d600393192904f2098443e | 60d5fabf3cb70946c077f221 | 60d600393192904f2098442f | 
 | 60d600393192904f2098443f | 60d5fabf3cb70946c077f221 | 60d600393192904f2098442f | 
 | 60d600393192904f20984440 | 60d5fabf3cb70946c077f221 | 60d600393192904f2098442f | 
-
-## My Comments
-- I assumed the averageScore should be to 1 dp
-- I assumed the request body for saving a new session should contain a key 'status diff'
-- I used mongoDBs ObjectID as IDs not a uuid
+|                          |                          |                          |
+| 60d600393192904f20984442 | 60d5fabf3cb70946c077f223 | 60d600393192904f20984430 | 
+| 60d600393192904f20984443 | 60d5fabf3cb70946c077f223 | 60d600393192904f20984430 | 
+| 60d600393192904f20984444 | 60d5fabf3cb70946c077f223 | 60d600393192904f20984430 | 
+|                          |                          |                          |
+| 60d600393192904f20984445 | 60d5fabf3cb70946c077f222 | 60d600393192904f20984430 | 
+| 60d600393192904f20984446 | 60d5fabf3cb70946c077f222 | 60d600393192904f20984430 | 
+| 60d600393192904f20984447 | 60d5fabf3cb70946c077f222 | 60d600393192904f20984430 | 
+|                          |                          |                          |
+| 60d7427c21c21217a00533b2 | 60d5fabf3cb70946c077f222 | 60d600393192904f2098442f | 
+| 60d7427c21c21217a00533b3 | 60d5fabf3cb70946c077f222 | 60d600393192904f2098442f | 
+| 60d7427c21c21217a00533b4 | 60d5fabf3cb70946c077f222 | 60d600393192904f2098442f | 
--- seed.js
+++ seed.js
@@ -21,52 +21,67 @@ mongoose.connect('mongodb+srv://testUser:S8c42xHbXB6MQem@cluster0.csliv.mongodb.
 // chip.save();
 
 // courses
-var maths = new Course({name: "Maths"});
-maths.save();
-var english = new Course({name: "English"});
-english.save();
+// var maths = new Course({name: "Maths"});
+// maths.save();
+// var english = new Course({name: "English"});
+// english.save();
 
 // sessions
-User.findOne({name:"Alice"})
+// User.findOne({name:"Alice"})
+// .then(data=> {
+//     let userId = data._id;
+//     Course.findOne({name: "Maths"})
+//     .then(data=> {
+//         let courseId = data._id;
+//         Session.insertMany([
+//             {userId: userId, courseId: courseId, totalModulesStudied: 11, averageScore: 91, timeStudied: 60*1000*180},
+//             {userId: userId, courseId: courseId, totalModulesStudied: 20, averageScore: 71, timeStudied: 60*1000*73},
+//             {userId: userId, courseId: courseId, totalModulesStudied: 5, averageScore: 61, timeStudied: 60*1000*120},
+//         ]).then(console.log("saved"));
+//     }) 
+    
+// });
+
+// User.findOne({name:"Chip"})
+// .then(data=> {
+//     let userId = data._id;
+//     Course.findOne({name: "English"})
+//     .then(data=> {
+//         let courseId = data._id;
+//         Session.insertMany([
+//             {userId: userId, courseId: courseId, totalModulesStudied: 3, averageScore: 50, timeStudied: 60*1000*10},
+//             {userId: userId, courseId: courseId, totalModulesStudied: 1, averageScore: 99, timeStudied: 60*1000*5},
+//             {userId: userId, courseId: courseId, totalModulesStudied: 19, averageScore: 82, timeStudied: 60*1000*120},
+//         ]).then(console.log("saved"));
+//     }) 
+    
+// })
+
+// User.findOne({name:"Bob"})
+// .then(data=> {
+//     let userId = data._id;
+//     Course.findOne({name: "English"})
+//     .then(data=> {
+//         let courseId = data._id;
+//         Session.insertMany([
+//             {userId: userId, courseId: courseId, totalModulesStudied: 12, averageScore: 71, timeStudied: 60*1000*100},
+//             {userId: userId, courseId: courseId, totalModulesStudied: 5, averageScore: 64, timeStudied: 60*1000*20},
+//             {userId: userId, courseId: courseId, totalModulesStudied: 6, averageScore: 63, timeStudied: 60*1000*60},
+//         ]).then(console.log("saved"));
+//     }) 
+    
+// })
+
+User.findOne({name:"Bob"})
 .then(data=> {
     let userId = data._id;
     Course.findOne({name: "Maths"})
     .then(data=> {
         let courseId = data._id;
         Session.insertMany([
-            {userId: userId, courseId: courseId, totalModulesStudied: 11, averageScore: 91, timeStudied: 60*1000*180},
-            {userId: userId, courseId: courseId, totalModulesStudied: 20, averageScore: 71, timeStudied: 60*1000*73},
-            {userId: userId, courseId: courseId, totalModulesStudied: 5, averageScore: 61, timeStudied: 60*1000*120},
-        ]).then(console.log("saved"));
-    }) 
-    
-});
-
-User.findOne({name:"Chip"})
-.then(data=> {
-    let userId = data._id;
-    Course.findOne({name: "English"})
-    .then(data=> {
-        let courseId = data._id;
-        Session.insertMany([
-            {userId: userId, courseId: courseId, totalModulesStudied: 3, averageScore: 50, timeStudied: 60*1000*10},
-            {userId: userId, courseId: courseId, totalModulesStudied: 1, averageScore: 99, timeStudied: 60*1000*5},
-            {userId: userId, courseId: courseId, totalModulesStudied: 19, averageScore: 82, timeStudied: 60*1000*120},
-        ]).then(console.log("saved"));
-    }) 
-    
-})
-
-User.findOne({name:"Bob"})
-.then(data=> {
-    let userId = data._id;
-    Course.findOne({name: "English"})
-    .then(data=> {
-        let courseId = data._id;
-        Session.insertMany([
-            {userId: userId, courseId: courseId, totalModulesStudied: 12, averageScore: 71, timeStudied: 60*1000*100},
-            {userId: userId, courseId: courseId, totalModulesStudied: 5, averageScore: 64, timeStudied: 60*1000*20},
-            {userId: userId, courseId: courseId, totalModulesStudied: 6, averageScore: 63, timeStudied: 60*1000*60},
+            {userId: userId, courseId: courseId, totalModulesStudied: 3, averageScore: 85, timeStudied: 60*1000*100},
+            {userId: userId, courseId: courseId, totalModulesStudied: 3, averageScore: 90, timeStudied: 60*1000*20},
+            {userId: userId, courseId: courseId, totalModulesStudied: 17, averageScore: 87, timeStudied: 60*1000*60},
         ]).then(console.log("saved"));
     }) 
     
